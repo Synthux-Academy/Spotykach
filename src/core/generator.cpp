cf5a074e2b62b79f121766ad1c495a67ecbbbdb4
Vladislav Litvinenko <thatvlad@icloud.com>
2026-05-13 12:53:20 +0200

    Ignore

--- src/core/generator.cpp
+++ src/core/generator.cpp
@@ -106,13 +106,13 @@ void Generator::apply_dimensions()
       break;
   }
   
-  if (_slice_points_count > 0) { /* pre-sliced */
+  auto mode = Config::dynamic().cue_size_mode(ref);
+  using CSM = Config::CueSizeMode;
+  if (mode != CSM::ignore && _slice_points_count > 0) { /* pre-sliced */
     auto last_point_idx = _slice_points_count - 1;
     auto start_idx = static_cast<size_t>(std::round(norm_start * last_point_idx));
     abs_start = _slice_points[start_idx];
 
-    auto mode = Config::dynamic().cue_size_mode(ref);
-    using CSM = Config::CueSizeMode;
     if ((_alt_size && mode == CSM::free) || (!_alt_size && mode == CSM::snap)) {
       auto end_idx = static_cast<size_t>(std::round((norm_start + norm_size) * last_point_idx));
       if (end_idx == start_idx) end_idx += 1;
